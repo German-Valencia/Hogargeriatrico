b4eedcf5d1066469b8808eff24a86e8e6a9a1bc5
Germán Alberto Valencia Torres <tu-usuario@users.noreply.github.com>
2024-11-22 12:18:03 -0500

    iniciando media queries

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,12 +27,15 @@ addEventOnElements(navTogglers, "click", toggleNav);
 
 
 const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]")
 
 const activeElementOnScroll = function () {
   if (window.scrollY > 100) {
-    header.classList.add("active")
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
   } else {
-    header.classList.remove("active")
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
   }
 }
 
