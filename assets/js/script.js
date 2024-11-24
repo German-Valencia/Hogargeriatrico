738ba25cb4d301787449f46da3cd5423e30be493
Germán Alberto Valencia Torres <tu-usuario@users.noreply.github.com>
2024-11-24 09:32:35 -0500

    Final

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,9 +25,8 @@ const toggleNav = function () {
 
 addEventOnElements(navTogglers, "click", toggleNav);
 
-
 const header = document.querySelector("[data-header]");
-const backTopBtn = document.querySelector("[data-back-top-btn]")
+const backTopBtn = document.querySelector("[data-back-top-btn]");
 
 const activeElementOnScroll = function () {
   if (window.scrollY > 100) {
@@ -37,7 +36,25 @@ const activeElementOnScroll = function () {
     header.classList.remove("active");
     backTopBtn.classList.remove("active");
   }
-}
+};
 
 window.addEventListener("scroll", activeElementOnScroll);
 
+const revealElements = document.querySelectorAll("[data-reveal]");
+
+const revealElementOnScroll = function () {
+  for (let i = 0, len = revealElements.length; i < len; i++) {
+    if (
+      revealElements[i].getBoundingClientRect().top <
+      window.innerHeight / 1.15
+    ) {
+      revealElements[i].classList.add("revealed");
+    } else {
+      revealElements[i].classList.remove("revealed");
+    }
+  }
+};
+
+window.addEventListener("scroll", revealElementOnScroll);
+
+window.addEventListener("load", revealElementOnScroll);
